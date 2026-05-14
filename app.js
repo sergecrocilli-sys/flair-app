758863d5d2865a6850179438b7f0b8c3a2f6af1d
sergecrocilli-sys <serge.crocilli@gmail.com>
2026-05-14 13:51:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -402,7 +402,7 @@ if (
 ) {
   score += 25;
 
-  type_signal = 'qualite_rappel_conso';
+  type_signal = 'autre';
 
   raison_score =
     "Contexte qualité sensible détecté (rappel conso / contamination / corps étranger).";
